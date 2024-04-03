0c23333f7e7ca5102dbb4a20dd53e8b1f10ada4d
miannetopokki <hugo.sasianturi@gmail.com>
2024-04-04 06:44:35 +0700

    feat : progress in fat32.c

--- src/fat32.c
+++ src/fat32.c
@@ -4,6 +4,7 @@
 #include "header/stdlib/string.h"
 #include "header/filesystem/fat32.h"
 
+static struct FAT32DriverState driver_state;
 const uint8_t fs_signature[BLOCK_SIZE] = {
     'C',
     'o',
@@ -110,10 +111,152 @@ uint32_t cluster_to_lba(uint32_t cluster)
  * @param name               8-byte char for directory name
  * @param parent_dir_cluster Parent directory cluster number
  */
-// void init_directory_table(struct FAT32DirectoryTable *dir_table, char *name, uint32_t parent_dir_cluster)
-// {
-//     for (int i = 0; i < 8; i++)
-//     {
-//         dir_table->table[0].name[i] = name[i];
-//     }
-// }
+void init_directory_table(struct FAT32DirectoryTable *dir_table, char *name, uint32_t parent_dir_cluster)
+{
+
+    struct FAT32DirectoryEntry *self_entry = &(dir_table->table[0]);
+    for (int i = 0; i < 8; i++)
+    {
+        self_entry->name[i] = name[i];
+    }
+    self_entry->attribute = ATTR_SUBDIRECTORY;
+    self_entry->user_attribute = UATTR_NOT_EMPTY;
+    self_entry->cluster_low = ROOT_CLUSTER_NUMBER;
+    self_entry->cluster_high = (ROOT_CLUSTER_NUMBER >> 16);
+
+    struct FAT32DirectoryEntry *parent_entry = &(dir_table->table[1]);
+    parent_entry->name[0] = '.';
+    parent_entry->name[1] = '.';
+    for (int i = 2; i < 8; i++)
+    {
+        parent_entry->name[i] = ' ';
+    }
+    parent_entry->attribute = ATTR_SUBDIRECTORY;
+    parent_entry->user_attribute = UATTR_NOT_EMPTY;
+    parent_entry->cluster_low = parent_dir_cluster;
+    parent_entry->cluster_high = (parent_dir_cluster >> 16);
+}
+/**
+ * Checking whether filesystem signature is missing or not in boot sector
+ *
+ * @return True if memcmp(boot_sector, fs_signature) returning inequality
+ */
+
+bool is_empty_storage(void)
+{
+    uint8_t bootsector[BLOCK_SIZE];
+    read_blocks(bootsector, 0, 1);
+    return memcmp(bootsector, fs_signature, BLOCK_SIZE);
+}
+
+/**
+ * Create new FAT32 file system. Will write fs_signature into boot sector and
+ * proper FileAllocationTable (contain CLUSTER_0_VALUE, CLUSTER_1_VALUE,
+ * and initialized root directory) into cluster number 1
+ */
+void create_fat32(void)
+{
+    uint8_t bootsector[BLOCK_SIZE];
+    memset(bootsector, 0, BLOCK_SIZE);
+    memcpy(bootsector, fs_signature, BLOCK_SIZE);
+    write_blocks(bootsector, 0, 1);
+    driver_state.fat_table.cluster_map[0] = CLUSTER_0_VALUE;
+    driver_state.fat_table.cluster_map[1] = CLUSTER_1_VALUE;
+    driver_state.fat_table.cluster_map[2] = FAT32_FAT_END_OF_FILE;
+
+    write_clusters(driver_state.fat_table.cluster_map, 0, 1);
+}
+
+/**
+ * Initialize file system driver state, if is_empty_storage() then create_fat32()
+ * Else, read and cache entire FileAllocationTable (located at cluster number 1) into driver state
+ */
+void initialize_filesystem_fat32(void)
+{
+    if (is_empty_storage())
+    {
+        create_fat32()
+    }
+    else
+    {
+        memset(driver_state.fat_table.cluster_map, 0, CLUSTER_SIZE);
+        read_clusters(driver_state.fat_table.cluster_map, 1, 1);
+    }
+}
+
+/**
+ * Write cluster operation, wrapper for write_blocks().
+ * Recommended to use struct ClusterBuffer
+ *
+ * @param ptr            Pointer to source data
+ * @param cluster_number Cluster number to write
+ * @param cluster_count  Cluster count to write, due limitation of write_blocks block_count 255 => max cluster_count = 63
+ */
+void write_clusters(const void *ptr, uint32_t cluster_number, uint8_t cluster_count)
+{
+    write_blocks(ptr, cluster_number * 4, cluster_count * CLUSTER_BLOCK_COUNT);
+}
+/**
+ * Read cluster operation, wrapper for read_blocks().
+ * Recommended to use struct ClusterBuffer
+ *
+ * @param ptr            Pointer to buffer for reading
+ * @param cluster_number Cluster number to read
+ * @param cluster_count  Cluster count to read, due limitation of read_blocks block_count 255 => max cluster_count = 63
+ */
+void read_clusters(void *ptr, uint32_t cluster_number, uint8_t cluster_count)
+{
+    read_blocks(ptr, cluster_number * 4, cluster_count * CLUSTER_BLOCK_COUNT);
+}
+
+/* -- CRUD Operation -- */
+
+/**
+ *  FAT32 Folder / Directory read
+ *
+ * @param request buf point to struct FAT32DirectoryTable,
+ *                name is directory name,
+ *                ext is unused,
+ *                parent_cluster_number is target directory table to read,
+ *                buffer_size must be exactly sizeof(struct FAT32DirectoryTable)
+ * @return Error code: 0 success - 1 not a folder - 2 not found - -1 unknown
+ */
+int8_t read_directory(struct FAT32DriverRequest request)
+{
+    if (request.buffer_size != sizeof(struct FAT32DirectoryTable))
+    {
+        return -1;
+    }
+    struct FAT32DirectoryTable dir_table;
+    read_clusters(&dir_table, request.parent_cluster_number, -1);
+    for (int i = 0; i < 64; i++)
+    {
+        if (memcmp(dir_table.table[i].name, request.name, 8) == 0)
+        {
+        }
+    }
+}
+
+/**
+ * FAT32 read, read a file from file system.
+ *
+ * @param request All attribute will be used for read, buffer_size will limit reading count
+ * @return Error code: 0 success - 1 not a file - 2 not enough buffer - 3 not found - -1 unknown
+ */
+int8_t read(struct FAT32DriverRequest request);
+
+/**
+ * FAT32 write, write a file or folder to file system.
+ *
+ * @param request All attribute will be used for write, buffer_size == 0 then create a folder / directory
+ * @return Error code: 0 success - 1 file/folder already exist - 2 invalid parent cluster - -1 unknown
+ */
+int8_t write(struct FAT32DriverRequest request);
+
+/**
+ * FAT32 delete, delete a file or empty directory (only 1 DirectoryEntry) in file system.
+ *
+ * @param request buf and buffer_size is unused
+ * @return Error code: 0 success - 1 not found - 2 folder is not empty - -1 unknown
+ */
+int8_t delete(struct FAT32DriverRequest request);
--- src/kernel.c
+++ src/kernel.c
@@ -29,17 +29,17 @@
 //     b += 1;
 // }
 
-void kernel_setup(void)
-{
-    load_gdt(&_gdt_gdtr);
-    pic_remap();
-    initialize_idt();
-    framebuffer_clear();
-    framebuffer_set_cursor(0, 0);
-    __asm__("int $0x4");
-    while (true)
-        ;
-}
+// void kernel_setup(void)
+// {
+//     load_gdt(&_gdt_gdtr);
+//     pic_remap();
+//     initialize_idt();
+//     framebuffer_clear();
+//     framebuffer_set_cursor(0, 0);
+//     __asm__("int $0x4");
+//     while (true)
+//         ;
+// }
 // void kernel_setup(void)
 // {
 //     load_gdt(&_gdt_gdtr);
@@ -56,19 +56,22 @@ void kernel_setup(void)
 //         ;
 // }
 
-//  void kernel_setup(void) {
-//     load_gdt(&_gdt_gdtr);
-//     pic_remap();
-//     initialize_idt();
-//     activate_keyboard_interrupt();
-//     framebuffer_clear();
-//     framebuffer_set_cursor(0, 0);
-        
-//     int col = 0;
-//     keyboard_state_activate();
-//     while (true) {
-//          char c;
-//          get_keyboard_buffer(&c);
-//          if (c) framebuffer_write(0, col++, c, 0xF, 0);
-//     }
-// }
+void kernel_setup(void)
+{
+    load_gdt(&_gdt_gdtr);
+    pic_remap();
+    initialize_idt();
+    activate_keyboard_interrupt();
+    framebuffer_clear();
+    framebuffer_set_cursor(0, 0);
+
+    int col = 0;
+    keyboard_state_activate();
+    while (true)
+    {
+        char c;
+        get_keyboard_buffer(&c);
+        if (c)
+            framebuffer_write(0, col++, c, 0xF, 0);
+    }
+}
