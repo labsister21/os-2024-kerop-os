2e1d833eeb308ff283704e10133c0e6fc40f9e48
miannetopokki <hugo.sasianturi@gmail.com>
2024-05-10 19:52:20 +0700

    feat:cd(50%)

--- src/user-shell.c
+++ src/user-shell.c
@@ -4,10 +4,8 @@
 #include "header/stdlib/string.h"
 #include "header/driver/disk.h"
 #include "header/driver/keyboard.h"
-uint32_t DIR_NUMBER_STACK[32] = {2};
-char DIR_NAME_STACK[32][8] = {"ROOT\0\0\0\0"};
-uint8_t DIR_STACK_LENGTH = 1;
 
+#define MAX_DIR_STACK_SIZE 32
 #define BLACK 0x00
 #define DARK_BLUE 0x01
 #define DARK_GREEN 0x2
@@ -47,7 +45,7 @@ char *custom_strchr(const char *str, int c)
         }
         str++;
     }
- 
+
     return NULL;
 }
 char *strtok(char *str, const char *delim)
@@ -55,17 +53,15 @@ char *strtok(char *str, const char *delim)
     static char *next_token = NULL;
     char *token_start;
 
-
     if (!str)
     {
         str = next_token;
         if (!str)
         {
-            return NULL; 
+            return NULL;
         }
     }
 
-   
     while (*str && custom_strchr(delim, *str))
     {
         str++;
@@ -76,7 +72,6 @@ char *strtok(char *str, const char *delim)
         return NULL;
     }
 
- 
     token_start = str;
     while (*str && !custom_strchr(delim, *str))
     {
@@ -84,7 +79,7 @@ char *strtok(char *str, const char *delim)
     }
     if (*str)
     {
-        *str = '\0'; 
+        *str = '\0';
         next_token = str + 1;
     }
     else
@@ -100,13 +95,11 @@ int atoi(const char *str)
     int result = 0;
     int sign = 1;
 
-  
     while (*str == ' ' || (*str >= 9 && *str <= 13))
     {
         str++;
     }
 
-  
     if (*str == '-')
     {
         sign = -1;
@@ -126,38 +119,91 @@ int atoi(const char *str)
     return result * sign;
 }
 
-
 // Kalo udah byte gede banget, strcmp kurang efektif, mending memcmp aja -hugo
 int strcmp(char *s1, char *s2)
 {
     int i = 0;
-   
-    do{
-        if (s1[i] != s2[i] )
+
+    do
+    {
+        if (s1[i] != s2[i])
         {
             return s1[i] - s2[i];
         }
         i++;
-    } while (s2[i] != '\n' || s2[i] != '\0');
+    } while (s1[i] != '\0');
 
     return 0;
-
 }
 
 void mkdir_command(char *dirname, uint32_t parent_cluster_number)
 {
-    
-    struct FAT32DriverRequest request;
-    request.buf = NULL;              
-    memcpy(request.name, dirname, 8); 
-    memcpy(request.ext, "\0\0\0", 3);
-    request.parent_cluster_number = parent_cluster_number;
-    request.buffer_size = 0; 
 
-    
+    struct FAT32DriverRequest request;
+    request.buf = NULL;
+    for (int8_t i = 0; i < 8; i++)
+    {
+        request.name[i] = dirname[i];
+    }
+    for (int8_t i = 0; i < 3; i++)
+    {
+        request.ext[i] = '\0';
+    }
+    request.parent_cluster_number = parent_cluster_number;
+    request.buffer_size = 0;
+
     syscall_user(2, (uint32_t)&request, 0, 0);
 }
-void exec_command(char* buff)
+void cede(char *dirname, uint32_t *dir_stack, uint8_t *dir_stack_index)
+{
+    struct FAT32DirectoryTable req_table;
+    struct FAT32DriverRequest request = {
+        .buf = &req_table,
+        .parent_cluster_number = dir_stack[*dir_stack_index],
+        .ext = "\0\0\0",
+        .buffer_size = 0,
+    };
+    if (strcmp(dirname, "..") == 0)
+    {
+        if (*dir_stack_index <= 0)
+        {
+            return;
+        }
+        else
+        {
+            (*dir_stack_index)--;
+        }
+    }
+    else if (strcmp(dirname, ".") == 0)
+    {
+        return;
+    }
+    else
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            request.name[i] = dirname[i];
+        }
+
+        int8_t retcode;
+        syscall_user(1, (uint32_t)&request, (uint32_t)&retcode, 0);
+
+        if (retcode != 0)
+        {
+            char err[18] = "INVALID DIRECTORY\n";
+            syscall_user(6, (uint32_t)err , 18, RED);
+            return;
+        }
+
+        (*dir_stack_index)++;
+        dir_stack[*dir_stack_index] = req_table.table[0].cluster_high << 16 | req_table.table[0].cluster_low;
+
+        
+        dir_stack[*dir_stack_index] = dir_stack[*dir_stack_index];
+    }
+}
+
+void exec_command(char *buff, uint32_t *dir_stack, uint8_t *dir_stack_index)
 {
     // Parse the input buffer for command and arguments
     char *args[MAX_ARGS];
@@ -175,68 +221,74 @@ void exec_command(char* buff)
     char cat[3] = "cat";
     char cls[3] = "cls";
 
-    
-
-    if (memcmp(args[0],cd,2) == 0)
+    if (strcmp(args[0], cd) == 0)
     {
-        // Handle cd command
+        char *dirname = args[1];
+        cede(dirname, dir_stack, dir_stack_index);
     }
-    else if (memcmp(args[0],ls,2) == 0)
+    else if (strcmp(args[0], ls) == 0)
     {
         // Handle ls command
     }
-    else if (memcmp(args[0],mkdir,5) == 0)
+    else if (strcmp(args[0], mkdir) == 0)
     {
         if (argc >= 2)
         {
             char *dirname = args[1];
-            uint32_t parent_cluster_number = ROOT_CLUSTER_NUMBER;
-            mkdir_command(dirname, parent_cluster_number);
+            mkdir_command(dirname, dir_stack[*dir_stack_index]);
         }
         else
         {
             char errorcode[33] = "[ERROR]: Usage: mkdir <dirname>\n";
-            syscall_user(6, (uint32_t) errorcode, 33, RED);
+            syscall_user(6, (uint32_t)errorcode, 33, RED);
         }
     }
-    else if (memcmp(args[0],cat,3) == 0)
+    else if (strcmp(args[0], cat) == 0)
     {
         // Handle cat command
     }
-    else if(memcmp(args[0],cls,3) == 0){
-        syscall_user(9,0,0,0);
+    else if (strcmp(args[0], cls) == 0)
+    {
+        syscall_user(9, 0, 0, 0);
     }
-
 
     else
     {
         char errorcode2[28] = "[ERROR]: Invalid Command !\n";
-        syscall_user(6, (uint32_t) errorcode2, 28, RED);
+        syscall_user(6, (uint32_t)errorcode2, 28, RED);
     }
-    memset(buff, 0, MAX_INPUT_BUFFER);
+    for ( int8_t i = 0; i < MAX_INPUT_BUFFER; i++)
+    {
+        buff[i] = 0x0;
+    }
     char intro[12] = "kerop-os $ ";
     syscall_user(6, (uint32_t)intro, 12, GREEN);
-    
 }
+
 int main(void)
 {
-        
+    uint32_t DIR_NUMBER_STACK[MAX_DIR_STACK_SIZE] = {ROOT_CLUSTER_NUMBER};
+    uint8_t DIR_STACK_INDEX = 0;
+
     syscall_user(7, 0, 0, 0);
     int i = 0;
     char buff[MAX_INPUT_BUFFER];
-    memset(buff, 0, MAX_INPUT_BUFFER);
     char intro[12] = "kerop-os $ ";
     syscall_user(6, (uint32_t)intro, 12, GREEN);
     while (true)
     {
-        // char intro[10] = "kerop-os$";
-        syscall_user(4, (uint32_t)buff,(uint32_t) i, 0);
-        syscall_user(5, (uint32_t)buff+i, 0xF, 0);
-        if (buff[i]!='\0'){
-            if (buff[i]=='\n'){
-                exec_command(buff);
+        syscall_user(4, (uint32_t)buff, (uint32_t)i, 0);
+        syscall_user(5, (uint32_t)buff + i, 0xF, 0);
+        if (buff[i] != '\0')
+        {
+            if (buff[i] == '\n')
+            {
+                buff[i] = 0x0;
+                exec_command(buff, DIR_NUMBER_STACK, &DIR_STACK_INDEX); // Pass the address of DIR_STACK_INDEX
                 i = 0;
-            }else{
+            }
+            else
+            {
                 i++;
             }
         }
